a24e17a97d08d58d40493cfb2910a4d97c95f151
maltael <www.galkin_daniil.ru@mail.ru>
2026-02-01 14:16:32 +0700

    небольшие комменты с урока

--- src/main/java/org/example/Security/security/JwtAuthenticationFilter.java
+++ src/main/java/org/example/Security/security/JwtAuthenticationFilter.java
@@ -17,7 +17,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 import java.util.List;
 @Component
-public class JwtAuthenticationFilter extends OncePerRequestFilter {
+public class JwtAuthenticationFilter extends OncePerRequestFilter {//срабатывает для каждого запроса
 
     private final JwtService jwtService;
     @Autowired
--- src/main/java/org/example/Security/security/JwtService.java
+++ src/main/java/org/example/Security/security/JwtService.java
@@ -12,7 +12,12 @@ public class JwtService {
 
     private static final long ACCESS_EXP = 1000 * 60 * 15;      // 15 мин
     private static final long REFRESH_EXP = 1000 * 60 * 60 * 24; // 24 часа
-
+    //по идее секретный ключ нужно хранить в программе Hashicorp Vault откуда при запуске она будет подтягиваться из БД или иного места
+    // и никогда случайно не попадет в github или не стащится декомпилятором твоего приложения
+//<dependency>
+//<groupId>org.springframework.cloud</groupId>
+//<artifactId>spring-cloud-starter-vault-config</artifactId>
+//</dependency>
     private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
     public String generateAccessToken(String username, String role) {
